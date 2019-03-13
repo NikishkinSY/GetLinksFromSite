8e5c45509b80bc722713acba2a8e601b2595bd32
Sergey Nikishkin <sergey@gohopscotch.com>
2019-03-13 11:20:46 +0300

    base project with link count

---
+++ LinkCount/Program.cs
@@ -0,0 +1,67 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkCount
+{
+    class Program
+    {
+        public static string BaseAddress = @"http://instrument33.ru";
+        public static int MaxLoopCount = 1;
+        public static int LoopCount;
+        public static StringComparer StringComparer = new StringComparer();
+
+        static void Main(string[] args)
+        {
+            var allPaths = GetPathsInLoop(new List<string> {"/"}).ToList();
+
+        }
+
+        public static IEnumerable<string> GetPathsInLoop(IEnumerable<string> paths)
+        {
+            if (++LoopCount > MaxLoopCount)
+            {
+                return new List<string>();
+            }
+            
+            var tasks = from path in paths
+                        select Task.Run(async () => await GetPathsAsync(path));
+            Task.WaitAll(tasks.ToArray());
+
+            var allPaths = new List<string>();
+            foreach (var task in tasks)
+            {
+                allPaths.AddRange(task.Result);
+            }
+
+            var newPaths = allPaths.Distinct(StringComparer).Except(paths, StringComparer);
+            var getNewPaths = GetPathsInLoop(newPaths);
+
+            return paths.Concat(newPaths).Concat(getNewPaths);
+        }
+
+        public static async Task<IEnumerable<string>> GetPathsAsync(string path)
+        {
+            var web = new HtmlWeb();
+            var htmlDoc = await web.LoadFromWebAsync(BaseAddress + path);
+            var aTags = htmlDoc.DocumentNode.Descendants("a").ToList();
+            var listNewLinks = new List<string>();
+
+            foreach (var aTag in aTags)
+            {
+                var hrefAttr = aTag.GetAttributeValue("href", null);
+
+                if (!string.IsNullOrWhiteSpace(hrefAttr)
+                    && hrefAttr.StartsWith('/'))
+                {
+                    listNewLinks.Add(hrefAttr);
+                }
+            }
+
+            return listNewLinks.Distinct(StringComparer);
+        }
+
+
+    }
+}
---
+++ LinkCount/StringComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkCount
+{
+    public class StringComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string value1, string value2)
+        {
+            return string.Equals(value1, value2, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public int GetHashCode(string value)
+        {
+            return value.GetHashCode();
+        }
+    }
+}
